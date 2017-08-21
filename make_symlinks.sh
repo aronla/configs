d9dfd111afb2cc7c3938dc8f9eee4883a9cc128b
aronla <aron.lagerberg@seal-software.com>
2017-08-21 15:35:29 +0200

    Make backups of previous configs

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,10 +1,20 @@
+mkdir ~/config_backups
+
 # fish
+mv ~/.config/fish/config.fish ~/config_backups
+mv ~/.fish_aliases ~/config_backups
+mv ~/.fish_variables ~/config_backups
+
 ln -s ~/configs/config.fish ~/.config/fish/config.fish
 ln -s ~/configs/.fish_aliases ~/.fish_aliases
 ln -s ~/configs/.fish_variables ~/.fish_variables
 
 # spacemacs
+mv ~/.spacemacs ~/config_backups
+
 ln -s ~/configs/.spacemacs ~/.spacemacs
 
 # tmux
+mv ~/.tmux.conf ~/config_backups
+
 ln -s ~/configs/.tmux.conf .tmux.conf
