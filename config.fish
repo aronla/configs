c52c329aca4625d6fa5fa2691a99bc18916bc24a
aronla <aron.lagerberg@seal-software.com>
2017-08-21 15:08:38 +0200

    Initial commit

---
+++ config.fish
@@ -0,0 +1,4 @@
+source ~/.fish_aliases
+source ~/.fish_variables
+
+set fish_key_bindings fish_user_key_bindings
---
+++ make_symlinks.sh
@@ -0,0 +1,10 @@
+# fish
+ln -s ~/configs/config.fish ~/.config/fish/config.fish
+ln -s ~/configs/.fish_aliases ~/.fish_aliases
+ln -s ~/configs/.fish_variables ~/.fish_variables
+
+# spacemacs
+ln -s ~/configs/.spacemacs ~/.spacemacs
+
+# tmux
+ln -s ~/configs/.tmux.conf .tmux.conf
