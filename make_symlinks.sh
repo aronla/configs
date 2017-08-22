103ea6608315aa9b64e386e2d8b694ace4f3c54b
aronla <aron.lagerberg@seal-software.com>
2017-08-22 15:16:15 +0200

    Added fish_functions

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -4,10 +4,12 @@ mkdir ~/config_backups
 mv ~/.config/fish/config.fish ~/config_backups
 mv ~/.fish_aliases ~/config_backups
 mv ~/.fish_variables ~/config_backups
+mv ~/.config/fish/functions/fish_user_key_bindings.fish ~/config_backup
 
 ln -s ~/configs/config.fish ~/.config/fish/config.fish
 ln -s ~/configs/.fish_aliases ~/.fish_aliases
 ln -s ~/configs/.fish_variables ~/.fish_variables
+ln -s ~/configs/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish 
 
 # spacemacs
 mv ~/.spacemacs ~/config_backups
