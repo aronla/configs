8c67bfda46602f2ad75db7d0a890fa2697d8f546
Your Name <you@example.com>
2017-09-14 22:26:37 +0200

    Expanded make_symlinks

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 source ~/.fish_aliases
 source ~/.fish_variables
+source ~/.fish_local_variables
 
 set fish_key_bindings fish_user_key_bindings
--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,22 +1,50 @@
-mkdir ~/config_backups
+#!/usr/bin/env bash
+backup_dir=~/config_backups
+
+if [ ! -d "$backup_dir" ]
+then
+    mkdir ~/config_backups
+    echo made dir $backup_dir
+else
+    echo dir $backup_dir exists
+fi
+
+
+function move_to_backup {
+    if [ -e "$1" ]
+    then
+        mv $1 $backup_dir
+    fi
+}
+
+function create_symlink {
+   if [ -L "$2" ]
+   then
+       echo symlink for $2 already exists
+   else
+       ln -s $1 $2
+   fi
+}
 
 # fish
-mv ~/.config/fish/config.fish ~/config_backups
-mv ~/.fish_aliases ~/config_backups
-mv ~/.fish_variables ~/config_backups
-mv ~/.config/fish/functions/fish_user_key_bindings.fish ~/config_backup
+move_to_backup ~/.config/fish/config.fish
+move_to_backup ~/.fish_aliases
+move_to_backup ~/.fish_variables
+move_to_backup ~/.fish_local_variables
+move_to_backup ~/.config/fish/functions/fish_user_key_bindings.fish
 
-ln -s ~/configs/config.fish ~/.config/fish/config.fish
-ln -s ~/configs/.fish_aliases ~/.fish_aliases
-ln -s ~/configs/.fish_variables ~/.fish_variables
-ln -s ~/configs/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish 
+create_symlink ~/configs/config.fish ~/.config/fish/config.fish
+create_symlink ~/configs/.fish_aliases ~/.fish_aliases
+create_symlink ~/configs/.fish_variables ~/.fish_variables
+create_symlink ~/configs/.fish_local_variables ~/.fish_local_variables
+create_symlink ~/configs/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
 
 # spacemacs
-mv ~/.spacemacs ~/config_backups
+move_to_backup ~/.spacemacs
 
-ln -s ~/configs/.spacemacs ~/.spacemacs
+create_symlink ~/configs/.spacemacs ~/.spacemacs
 
 # tmux
-mv ~/.tmux.conf ~/config_backups
+move_to_backup ~/.tmux.conf
 
-ln -s ~/configs/.tmux.conf .tmux.conf
+create_symlink ~/configs/.tmux.conf ~/.tmux.conf
