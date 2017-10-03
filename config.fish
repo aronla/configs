d44c087af702cd577743a41dde7c705d0684bbb9
aron <aron>
2017-10-03 11:21:28 +0200

    added check for local variables file

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
 source ~/.fish_aliases
 source ~/.fish_variables
-source ~/.fish_local_variables
+if test -e ~/fish_local_variables
+  source ~/.fish_local_variables
+end
 
 set fish_key_bindings fish_user_key_bindings
