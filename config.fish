9346befb6cee85fc5578df9269b9e8bd87d1da11
aron <aron>
2017-10-03 11:30:52 +0200

    corrected bug

--- config.fish
+++ config.fish
@@ -1,7 +1,9 @@
 source ~/.fish_aliases
 source ~/.fish_variables
-if test -e ~/fish_local_variables
+if test -e ~/.fish_local_variables
   source ~/.fish_local_variables
+else
+  echo did not load local variables
 end
 
 set fish_key_bindings fish_user_key_bindings
