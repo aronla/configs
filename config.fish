6c9bce5aed731b0a674ab518395c03d85f9f8b39
aronla <aron.lagerberg@seal-software.com>
2018-03-07 08:02:26 +0100

    Updated configs

--- config.fish
+++ config.fish
@@ -7,3 +7,6 @@ else
 end
 
 set fish_key_bindings fish_user_key_bindings
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/aron/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/aron/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/aron/Downloads/google-cloud-sdk/path.fish.inc'; end; end
