40780e62e17ad0ed48d670207469761155e77594
aronla <aron.lagerberg@seal-software.com>
2018-04-04 14:57:05 +0200

    Updated fish keybindings to use C-f and C-g to accept autocompletions

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -2,4 +2,6 @@ function fish_user_key_bindings
   fish_vi_key_bindings
 
   bind -M insert -m default fd backward-char force-repaint
+  bind -M insert \cf forward-char
+  bind -M insert \cg forward-word
 end
