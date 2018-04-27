6159b1faebaf7ea5b73ead18c18efa91f3761c1e
aronla <aron.lagerberg@seal-software.com>
2018-04-27 08:42:42 +0200

    Added shortcuts for searching in history

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -4,4 +4,7 @@ function fish_user_key_bindings
   bind -M insert -m default fd backward-char force-repaint
   bind -M insert \cf forward-char
   bind -M insert \cg forward-word
+
+  bind -M insert \cR up-or-search
+  bind -M insert \cT down-or-search
 end
